1c679bbe1f763482c6da2f16d91b5c9ebd689729
yongmon01 <60520539+yongmon01@users.noreply.github.com>
2021-08-10 13:04:58 +0900

    함수 위치 재배치
    함수들 renderPlainText 함수 안으로 재배치..

--- src/Chapter1/chapter1.js
+++ src/Chapter1/chapter1.js
@@ -17,77 +17,76 @@ const plays = {
   othello: { name: "othello", type: "tragedy" },
 };
 
-function amountFor(aPerformance) {
-  let result = 0;
-  switch (playFor(aPerformance).type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 20) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-
-    default:
-      throw new Error("알수 없는 장르");
-  }
-  return result;
-}
-
-function playFor(aPerformace) {
-  return plays[aPerformace.playID];
-}
-
-function volumeCreditFor(aPerformace) {
-  let result = 0;
-  result += Math.max(aPerformace.audience - 30, 0);
-  if ("comedy" === playFor(aPerformace).type)
-    result += Math.floor(aPerformace.audience / 5);
-  return result;
-}
-
-// 책: function totalVolumeCredit(){
-function totalVolumeCredit(invoce) {
-  let result = 0;
-  for (let perf of invoce[0].performances) {
-    result += volumeCreditFor(perf);
-  }
-  return result;
-}
-
-function totalAmount(invoce) {
-  let result = 0;
-  for (let perf of invoce[0].performances) {
-    result += amountFor(perf);
-  }
-  return result;
-}
-
-function renderPlainText(data, invoce, plays) {
+function renderPlainText(data, plays) {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
-
   for (let perf of data.performances) {
     result += `${playFor(perf).name}: ${amountFor(perf) / 100} (${
       perf.audience
     })석 \n`;
   }
-  result += `총액: ${totalAmount(invoce) / 100}\n`;
-  result += `적립 포인트 : ${totalVolumeCredit(invoce)}점\n`;
+  result += `총액: ${totalAmount() / 100}\n`;
+  result += `적립 포인트 : ${totalVolumeCredit()}점\n`;
   return result;
+
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 20) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+
+      default:
+        throw new Error("알수 없는 장르");
+    }
+    return result;
+  }
+
+  function playFor(aPerformace) {
+    return plays[aPerformace.playID];
+  }
+
+  function volumeCreditFor(aPerformace) {
+    let result = 0;
+    result += Math.max(aPerformace.audience - 30, 0);
+    if ("comedy" === playFor(aPerformace).type)
+      result += Math.floor(aPerformace.audience / 5);
+    return result;
+  }
+
+  // 책: function totalVolumeCredit(){
+  function totalVolumeCredit() {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += volumeCreditFor(perf);
+    }
+    return result;
+  }
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
 }
 
 function statement(invoce, plays) {
   const statementData = {};
   statementData.customer = invoce[0].customer;
   statementData.performances = invoce[0].performances;
-  return renderPlainText(statementData, invoce, plays);
+  return renderPlainText(statementData, plays);
 }
 
 console.log(statement(invoces, plays));
