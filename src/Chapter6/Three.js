af3e820820e39cc6ad10fd0dd085dd4f7f8f5926
yongmon01 <60520539+yongmon01@users.noreply.github.com>
2021-08-31 18:38:19 +0900

    6.3 클래스 안에서

--- src/Chapter6/Three.js
+++ src/Chapter6/Three.js
@@ -1,9 +1,20 @@
-function price(order) {
-  //가격 = 기본가격 - 수량 할인 + 배송비
-  const basePrice = order.quantity * order.itemPrice;
-  const quantityDiscount =
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-  const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+class Order {
+  constructor(aRecord) {
+    this._data = aRecord;
+  }
 
-  return basePrice - quantityDiscount + shipping;
+  get quantity() {
+    return this._data.quantity;
+  }
+  get itemPrice() {
+    return this._data.itemPrice;
+  }
+
+  get price() {
+    return (
+      this.quantity * this.itemPrice -
+      Math.max(0, order.quantity - 500) * this.itemPrice * 0.05 +
+      Math.min(order.quantity * this.itemPrice * 0.1, 100)
+    );
+  }
 }
