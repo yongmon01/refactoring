6fabea65523cf6da4482592b70519095f848de0d
yongmon01 <60520539+yongmon01@users.noreply.github.com>
2021-08-31 17:58:15 +0900

    6.1 지역 변수를 사용할 때

--- src/Chapter6/one.js
+++ src/Chapter6/one.js
@@ -7,23 +7,28 @@ function printOwing(invoce) {
     outstanding += o.amount;
   }
 
-  const today = Clock.today;
-  invoce.dueDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDueDate() + 30
-  );
-
-  printDetails();
+  recordDueDate(invoce);
+  printDetails(invoce, outstanding);
 
   function printBanner() {
     console.log("***************");
     console.log("***고객 채무***");
     console.log("***************");
   }
-  function printDetails() {
-    console.log(`고객명: ${invoce.customer}`);
-    console.log(`채무액: ${outstanding}`);
-    console.log(`마감일: ${invoce.dueDate.toLocaleDateString()}`);
-  }
+}
+
+function printDetails(invoce, outstanding) {
+  console.log(`고객명: ${invoce.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoce.dueDate.toLocaleDateString()}`);
+}
+
+// invoce의 필드값을 수정할수있다
+function recordDueDate(invoce) {
+  const today = Clock.today;
+  invoce.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDueDate() + 30
+  );
 }
