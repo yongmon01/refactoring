d982b4a57c69d7c1265186270cc9d7df5b2b1c56
yongmon01 <60520539+yongmon01@users.noreply.github.com>
2021-08-31 20:08:44 +0900

    6.5 매개변수를 속성으로 바꾸기

--- src/Chapter6/five.js
+++ src/Chapter6/five.js
@@ -1,3 +1,9 @@
 function circumference(radius) {
   return 2 * Math.PI * radius;
 }
+
+function inNewEngland(aCustomer) {
+  return ["MA", "CT", "ME", "VT"].includes(aCustomer.address.state);
+}
+
+const newEnglanders = somCuostemers.filter((c) => inNewEngland(c));
