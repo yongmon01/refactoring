de1e8e6bd8aed701332eb5b612a9d1becbaa1431
yongmon01 <60520539+yongmon01@users.noreply.github.com>
2021-09-01 13:38:33 +0900

    chapter6.6 변수 캡슐화 하기

--- src/Chapter6/six.js
+++ src/Chapter6/six.js
@@ -1,4 +1,6 @@
 let defaultOwner = { firstName: "martin", lastName: "fouler" };
 
+// 참조하는 코드
 spaceship.owner = defaultOwner;
+// 갱신하는 코드
 defaultOwner = { firstName: "lebeca", lastName: "parsons" };
