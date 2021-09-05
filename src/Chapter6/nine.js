f702e4a2cb0ca3879d3e50ee3476e077c3576c29
hoyong <hoyong_lee@tmax.co.kr>
2021-09-05 14:16:40 +0900

    6.9 여러 함수를 클래스로 묶기

--- src/Chapter6/nine.js
+++ src/Chapter6/nine.js
@@ -1,5 +1,7 @@
 reading = {customer: 'ivan', quantity: 10, month:5, year:2017}
 
-function base(aReading){}
-function taxableCharge(aReading){}
-function calculateBaseCharge(aReading){}
+class Reading{
+    base(){}
+    taxableCharge(){}
+    calculateBaseCharge(){}
+}
