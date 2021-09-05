ada1c6eaeb7c3dc5f06970506823bff638c9ba0a
hoyong <hoyong_lee@tmax.co.kr>
2021-09-05 14:39:38 +0900

    6.10 여러 함수를 변환함수로 묶기

--- src/Chapter6/ten.js
+++ src/Chapter6/ten.js
@@ -1,2 +1,13 @@
-function base(aReading){}
-function taxableCharge(aReading){}
+// enrich: 본질은 같고 부가 정보만 덧붙이는 반환 함수라는뜻
+function enrichReading(original){
+    const result = _.cloneDeep(original)
+    result.baseCharge = calculateBaseCharge(result)
+    result.taxableCharge = Math.max(0, result.baseCharge - taxThreshold(result.year))
+    return result
+}
+
+function calculateBaseCharge(){}
+
+const rawReading = acquireReading()
+const aReading = enrichReading(rawReading)
+const taxableCharge = aReading.taxableCharge
