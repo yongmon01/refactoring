14a8fe7257edfe19b64a6b555f207157a9de22e2
yongmon01 <60520539+yongmon01@users.noreply.github.com>
2021-09-06 18:17:31 +0900

    6.11 계산 단계 분리

--- src/Chapter6/eleven.js
+++ src/Chapter6/eleven.js
@@ -5,6 +5,11 @@ function priceOrder(product, quantity, shippingMethod) {
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
+  const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+  return price;
+}
+
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
   const shippingPerCase =
     basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
