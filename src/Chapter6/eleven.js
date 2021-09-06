9ae2de6980bb297e53754e61449a9e6773fb473d
yongmon01 <60520539+yongmon01@users.noreply.github.com>
2021-09-06 18:48:21 +0900

    6.11 함수로 추출하기

--- src/Chapter6/eleven.js
+++ src/Chapter6/eleven.js
@@ -1,17 +1,21 @@
 // 상품 결재 금액을 계산 하는 코드
 function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+  const price = applyShipping(priceData, shippingMethod);
+  return price;
+}
+
+function calculatePricingData(product, quantity) {
   const basePrice = product.basePrice * quantity;
   const discount =
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
-  const priceData = {
+  return {
     basePrice: basePrice,
     quantity: quantity,
     discount: discount,
   };
-  const price = applyShipping(priceData, shippingMethod);
-  return price;
 }
 
 function applyShipping(priceData, shippingMethod) {
