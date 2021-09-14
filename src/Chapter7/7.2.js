fb394c238f86df2036023006c92114514f5614d9
hoyong <hoyong_lee@tmax.co.kr>
2021-09-14 17:14:02 +0900

    7.2 컬렉션 캡슐화 하기

--- src/Chapter7/7.2.js
+++ src/Chapter7/7.2.js
@@ -4,8 +4,17 @@ class Person{
         this._courses = []
     }
     get name(){return this._name}
-    get courses(){return this._courses}
-    set courses(aList){this._courses = aList}
+    // 복제본을 리턴 (slice)
+    get courses(){return this._courses.slice()}
+    addCourse(aCourse){
+        this._courses.push(aCourse)
+    }
+    removeCourse(aCourse, fnIfAbsent = () =>{throw new RangeError()}){
+        const index = this.courses.indexOf(aCourse)
+        if (index === -1) fnIfAbsent()
+        // splice: 원본 배열 자체를 수정한다.
+        else this._courses.splice(index, 1)
+    }
 }
 
 class Course{
