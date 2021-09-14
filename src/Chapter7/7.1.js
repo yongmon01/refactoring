00cbd253636fa3d18cd41cd666288ffd9b28b096
hoyong <hoyong_lee@tmax.co.kr>
2021-09-14 16:27:33 +0900

    7.1 간단한 레코드 캡슐화

--- src/Chapter7/7.1.js
+++ src/Chapter7/7.1.js
@@ -1,4 +1,12 @@
-const organization = {name:'ackme', country:'GB'}
+class Organization{
+    constructor(data){
+        this._name = data.name
+        this._country = data.country
+    }
+    get name(){return this._data.name}
+    set name(aString){this._data.name = aString}
+    get country(){return this._data.country}
+    set country(aCountryCode){this._data.country = aCountryCode}
+}
 
-console.log(organization.name)
-organization.name = 'newGB'
+const org = new Organization({name:'ackme', country:'GB'})
