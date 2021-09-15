60062e6693c1dd92905bd41c14784f00e8cd6d1b
yongmon01 <60520539+yongmon01@users.noreply.github.com>
2021-09-15 15:16:27 +0900

    7.3 Priority 객체 생성

--- src/Chapter7/7.3.js
+++ src/Chapter7/7.3.js
@@ -2,8 +2,24 @@ class Order {
   constructor(data) {
     this.priority = data.priority;
   }
+
+  get priorityString() {
+    return this._priority.toString();
+  }
+  set priority(aString) {
+    this._priority = new Priority(aString);
+  }
+}
+
+class Priority {
+  constructor(value) {
+    this._value = value;
+  }
+  toString() {
+    return this._value;
+  }
 }
 
 let highPriorityCount = orders.filter(
-  (o) => o.priority === "high" || o.priority === "rush"
+  (o) => o.priorityString === "high" || o.priorityString === "rush"
 ).length;
