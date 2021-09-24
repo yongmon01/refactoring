b0e002dfa815c636e7b57e45141fa856d5f7e05e
yongmon01 <60520539+yongmon01@users.noreply.github.com>
2021-09-24 13:38:48 +0900

    7.5 TelephoneNumber 클래스 추출

--- src/Chapter7/7.5.js
+++ src/Chapter7/7.5.js
@@ -1,5 +1,7 @@
 class Person {
-  constructor() {}
+  constructor() {
+      this._telephoneNumber = new TelephoneNumber()
+  }
 
   get name() {
     return this._name;
@@ -8,18 +10,26 @@ class Person {
     this._name = arg;
   }
   get telephoneNumber() {
-    return `(${this.officeAreaCode}) ${this.officeNumber}`;
+    return this._telephoneNumber.telephoneNumber;
   }
   get officeAreaCode() {
-    return this._officeAreaCode;
+    return this._telephoneNumber.officeAreaCode;
   }
   set officeAreaCode(arg) {
-    this._officeAreaCode = arg;
+    this._telephoneNumber._officeAreaCode = arg;
   }
   get officeNumber() {
-    return this._officeNumber;
+    return this._telephoneNumber._officeNumber;
   }
   set officeNumber(arg) {
-    this._officeNumber = arg;
+    this._telephoneNumber._officeNumber = arg;
   }
+}
+
+class TelephoneNumber{
+    constructor()
+
+    get officeAreaCode() {return this._officeAreaCode}
+    set officeAreaCode(arg) {this._officeAreaCode = arg}
+    get telephoneNumber() {return `(${this.officeAreaCode}) ${this.officeNumber}`}
 }
